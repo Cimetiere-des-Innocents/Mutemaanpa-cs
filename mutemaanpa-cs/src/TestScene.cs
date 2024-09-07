3a21500cb4b3edd7394f90e9f6dd039401384b0a
hortensia <824466875@qq.com>
2024-09-07 22:45:12 +0800

    add yarn spinner

--- mutemaanpa-cs/src/TestScene.cs
+++ mutemaanpa-cs/src/TestScene.cs
@@ -4,50 +4,50 @@ namespace Mutemaanpa;
 
 public partial class TestScene : Node3D
 {
-	[Export]
-	private EntitySpawner? playerSpawner;
+    [Export]
+    private EntitySpawner? playerSpawner;
 
-	private Player? player;
+    private Player? player;
 
-	public static DirAccess ToGameFS()
-	{
-		var dir = DirAccess.Open(".");
-		if (!dir.DirExists("m8a"))
-		{
-			dir.MakeDir("m8a");
-		}
-		dir.ChangeDir("m8a");
-		return dir;
-	}
+    public static DirAccess ToGameFS()
+    {
+        var dir = DirAccess.Open(".");
+        if (!dir.DirExists("m8a"))
+        {
+            dir.MakeDir("m8a");
+        }
+        dir.ChangeDir("m8a");
+        return dir;
+    }
 
-	public void SpawnPlayer()
-	{
-		player = playerSpawner?.SpawnEntity<Player>();
-		var dir = ToGameFS();
-		if (dir.FileExists("testSave.json"))
-		{
-			using var file = FileAccess.Open($"{dir.GetCurrentDir()}/testSave.json", FileAccess.ModeFlags.Read);
-			var content = file.GetAsText();
-			var parseResult = Json.ParseString(content);
-			(player! as Entity<Node3D>).Load(parseResult.As<SaveDict>());
-		}
-	}
+    public void SpawnPlayer()
+    {
+        player = playerSpawner?.SpawnEntity<Player>();
+        var dir = ToGameFS();
+        if (dir.FileExists("testSave.json"))
+        {
+            using var file = FileAccess.Open($"{dir.GetCurrentDir()}/testSave.json", FileAccess.ModeFlags.Read);
+            var content = file.GetAsText();
+            var parseResult = Json.ParseString(content);
+            (player! as Entity<Node3D>).Load(parseResult.As<SaveDict>());
+        }
+    }
 
-	public void SavePlayer()
-	{
-		if (player != null)
-		{
-			var dir = ToGameFS();
-			using var file = FileAccess.Open($"{dir.GetCurrentDir()}/testSave.json", FileAccess.ModeFlags.Write);
-			var saveDict = new SaveDict();
-			(player as Entity<Node3D>).Save(saveDict);
-			var saveJson = Json.Stringify(saveDict);
-			file.StoreString(saveJson);
-		}
-	}
+    public void SavePlayer()
+    {
+        if (player != null)
+        {
+            var dir = ToGameFS();
+            using var file = FileAccess.Open($"{dir.GetCurrentDir()}/testSave.json", FileAccess.ModeFlags.Write);
+            var saveDict = new SaveDict();
+            (player as Entity<Node3D>).Save(saveDict);
+            var saveJson = Json.Stringify(saveDict);
+            file.StoreString(saveJson);
+        }
+    }
 
-	public void QuitGame()
-	{
-		GetTree().Quit();
-	}
+    public void QuitGame()
+    {
+        GetTree().Quit();
+    }
 }
--- mutemaanpa-cs/src/game/character/Player.cs
+++ mutemaanpa-cs/src/game/character/Player.cs
@@ -5,31 +5,31 @@ namespace Mutemaanpa;
 [Entity]
 public partial class Player : Character
 {
-	public static readonly EntityType<Player> TYPE = EntityTypeUtil.FromScene<Player>("player", "res://scene/game/character/player.tscn");
+    public static readonly EntityType<Player> TYPE = EntityTypeUtil.FromScene<Player>("player", "res://scene/game/character/player.tscn");
 
-	private double lastDamaged = 0.0;
+    private double lastDamaged = 0.0;
 
-	public override void _PhysicsProcess(double delta)
-	{
-		base._PhysicsProcess(delta);
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
 
-		var input2d = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
-		var input = new Vector3(input2d.X, 0, input2d.Y);
+        var input2d = Input.GetVector("move_left", "move_right", "move_forward", "move_backward");
+        var input = new Vector3(input2d.X, 0, input2d.Y);
 
-		foreach (var kinematicCollision3D in this.GetSlideCollisions())
-		{
-			if (kinematicCollision3D.GetCollider() is RigidBody3D rigidBody && rigidBody.Name == "Unnis")
-			{
-				lastDamaged += delta;
-				if (lastDamaged > 0.5)
-				{
-					Hit(1.0f);
-					lastDamaged -= 0.5;
-				}
-			}
-		}
+        foreach (var kinematicCollision3D in this.GetSlideCollisions())
+        {
+            if (kinematicCollision3D.GetCollider() is RigidBody3D rigidBody && rigidBody.Name == "Unnis")
+            {
+                lastDamaged += delta;
+                if (lastDamaged > 0.5)
+                {
+                    Hit(1.0f);
+                    lastDamaged -= 0.5;
+                }
+            }
+        }
 
-		Velocity = GetVelocity(input);
-		MoveAndSlide();
-	}
+        Velocity = GetVelocity(input);
+        MoveAndSlide();
+    }
 }
